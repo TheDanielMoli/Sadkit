270bcef92142f143e4183d15290627449dcfde2c
Daniele Molinari <daniele.molinari@sadkit.com>
2019-01-02 00:14:18 +0100

    Accessories.

---
+++ www/sadkit.com/precache-manifest.7fbd832f8453e16fa0317de4eec80035.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "64ea36e13187cb0530ad",
+    "url": "/static/js/main.64ea36e1.chunk.js"
+  },
+  {
+    "revision": "c36b60957d33b899ce37",
+    "url": "/static/js/1.c36b6095.chunk.js"
+  },
+  {
+    "revision": "64ea36e13187cb0530ad",
+    "url": "/static/css/main.9e987c81.chunk.css"
+  },
+  {
+    "revision": "9218bda70beb69a2a839dfb3ddd9e3e3",
+    "url": "/index.html"
+  }
+];
